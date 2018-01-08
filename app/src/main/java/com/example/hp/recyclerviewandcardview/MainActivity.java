0d0b5f08fb9ff97bd299c9169779f07a43fef727
HP <harshitgupta323@gmail.com>
2018-01-08 12:09:53 +0530

    Test

--- app/src/main/java/com/example/hp/recyclerviewandcardview/MainActivity.java
+++ app/src/main/java/com/example/hp/recyclerviewandcardview/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Trying to change the code
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
